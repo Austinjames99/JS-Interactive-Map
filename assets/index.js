5c36ac8d9aa3adc379bc1c57c2d896b3ba1e9646
Austin James <austinjames@Austins-MacBook-Air.local>
2023-01-21 13:50:03 -0500

    part 2 done

--- assets/index.js
+++ assets/index.js
@@ -25,7 +25,18 @@ const myMap = {
 	},
 
 	// add business markers
+    addMarkers() {
+		for (var i = 0; i < this.businesses.length; i++) {
+		this.markers = L.marker([
+			this.businesses[i].lat,
+			this.businesses[i].long,
+		])
+			.bindPopup(`<p1>${this.businesses[i].name}</p1>`)
+			.addTo(this.map)
+		}
+	},
 }
+
 
 // get coordinates via geolocation api
 async function getCoords(){
@@ -36,22 +47,56 @@ async function getCoords(){
 }
 
 // get foursquare businesses
+async function getFoursquare(business) {
+	const options = {
+		method: 'GET',
+		headers: {
+		Accept: 'application/json',
+		Authorization: 'fsq387kjPRdCbUbYPpsu1AG6gJnjOar9lksPjqokDjt8Puo='
+		}
+	}
+	let limit = 5
+	let lat = myMap.coordinates[0]
+	let lon = myMap.coordinates[1]
+	let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let data = await response.text()
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
+}
 
 // process foursquare array
+function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.geocodes.main.latitude,
+			long: element.geocodes.main.longitude
+		};
+		return location
+	})
+	return businesses
+}
 
 
 // event handlers
 // window load
 window.onload = async () => {
 	const coords = await getCoords()
-	console.log(coords)
 	myMap.coordinates = coords
 	myMap.buildMap()
 }
 
 // business submit button
-document.getElementById('submit').addEventListener('click', async (event) => {
+
+const submit = document.getElementById('submit')
+submit.addEventListener('click', async (event) => {
 	event.preventDefault()
 	let business = document.getElementById('business').value
 	console.log(business)
 })
+
+
+
+
+
